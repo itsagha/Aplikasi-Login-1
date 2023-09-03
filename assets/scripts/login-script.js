329fa228d76ddb15625171633a62691922a532ce
Rasyid Muhammad Agha <121854795+itsagha@users.noreply.github.com>
2023-09-03 18:40:42 +0700

    Update login-script.js
    Mengganti komentar sesuai dengan format JSDoc.

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -5,21 +5,29 @@
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
  */
 
+/* Menambahkan variabel untuk setiap elemen view*/
 const loginFormElement = document.querySelector('#loginForm');
 const inputEmailElement = document.querySelector('#inputEmail');
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+/* Membuat variabel untuk menyimpan informasi email dan pasword */
 const expectedEmail = 'admin@dicoding.com';
 const expectedPassword = 'superpassword';
 
+/* Membuat aksi klik pada button */ 
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
 
+  // mendapatkan nilai dari masing masing input
   const email = inputEmailElement.value;
   const password = inputPasswordElement.value;
+
+  //memastikan bahwa email dan password sesuai dengan nilai yang tersimpan
   if (email == expectedEmail && password == expectedPassword) {
     goToHome();
+    // Jika sesuai maka akan ke halaman home
   } else {
+    //jika salah maka akan menampilkan informasi bahwa input salah
     showPopUp();
   }
 });
